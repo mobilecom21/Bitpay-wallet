5fc57fcad39d3dab33aac011a5274e84ffca133e
mobilecom21 <mobile.com21@yahoo.com>
2018-06-08 12:35:17 +0300

    Manage your bitcoin finances in one app with the secure, open source BitPay Wallet. Get up and running fast with bitcoin security, store and send funds anywhere, buy and sell bitcoin, and turn bitcoin into dollars with the BitPay Visa Card.

---
+++ cordova-js-src/platform.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+    id: 'ios',
+    bootstrap: function () {
+        // Attach the console polyfill that is iOS-only to window.console
+        // see the file under plugin/ios/console.js
+        require('cordova/modulemapper').clobbers('cordova/plugin/ios/console', 'window.console');
+
+        require('cordova/channel').onNativeReady.fire();
+    }
+};
